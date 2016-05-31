90fe0c47403d6508abf0bce67e2576ad35d17b2d
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-05-31 14:38:09 -0300

    Add check branch

--- lib/githelper.rb
+++ lib/githelper.rb
@@ -1,5 +1,21 @@
 require "githelper/version"
 
 module Githelper
-  # Your code goes here...
+  module_function
+
+  def check_branch(branch, base_branch)
+    branch_commits = Githelper.get_branch_commits(branch)
+    base_branch_commits = Githelper.get_branch_commits(base_branch)
+
+    unless branch_commits.include?(base_branch_commits.first)
+      puts "Need rebase"
+    end
+  end
+
+  def get_branch_commits(branch)
+    commits = `git log master`.split("\n")
+    commits = commits.collect{|line| line.scan(/commit (.*)/).flatten}
+    commits = commits.select{|line| !line.empty?}
+    return commits
+  end
 end
