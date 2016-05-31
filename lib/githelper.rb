092046da7e3ba986ea24fb7e33e3f3ad50a6b2e2
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-05-31 15:03:21 -0300

    Check if branch needs rebase

--- lib/githelper.rb
+++ lib/githelper.rb
@@ -3,13 +3,22 @@ require "githelper/version"
 module Githelper
   module_function
 
+  BRANCH_STATUS = [:need_rebase]
+
   def check_branch(branch, base_branch)
     branch_commits = Githelper.get_branch_commits(branch)
     base_branch_commits = Githelper.get_branch_commits(base_branch)
 
-    unless branch_commits.include?(base_branch_commits.first)
-      puts "Need rebase"
+    if need_rebase(branch_commits, base_branch_commits)
+      return BRANCH_STATUS[:need_rebase]
     end
+  end
+
+  def need_rebase(branch_commits, base_branch_commits)
+    need_rebase = !base_branch_commits.include?(branch_commits.first)
+    need_rebase &= !branch_commits.include?(base_branch_commits.first)
+
+    return need_rebase
   end
 
   def get_branch_commits(branch)
--- spec/githelper_spec.rb
+++ spec/githelper_spec.rb
@@ -4,4 +4,30 @@ describe Githelper do
   it 'has a version number' do
     expect(Githelper::VERSION).not_to be nil
   end
+
+  describe 'test need_rebase function' do
+    it 'branch with base commits not needs rebase' do
+      branch_commits = ['e', 'd', 'c', 'b', 'a']
+      base_branch_commits = ['d', 'c', 'b', 'a']
+
+      result = Githelper.need_rebase(branch_commits, base_branch_commits)
+      expect(result).to be false
+    end
+
+    it 'branch with same commits of base not needs rebase' do
+      branch_commits = ['e', 'd', 'c', 'b', 'a']
+      base_branch_commits = ['e', 'd', 'c', 'b', 'a']
+
+      result = Githelper.need_rebase(branch_commits, base_branch_commits)
+      expect(result).to be false
+    end
+
+    it 'branch without all base commits not needs rebase' do
+      branch_commits = ['e', 'c', 'b', 'a']
+      base_branch_commits = ['d', 'c', 'b', 'a']
+
+      result = Githelper.need_rebase(branch_commits, base_branch_commits)
+      expect(result).to be true
+    end
+  end
 end
