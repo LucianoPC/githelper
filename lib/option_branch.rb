d25064b8568d31ae0949706664fb3c64f70e3450
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-06-02 13:58:06 -0300

    Add options into option_branch

--- lib/option_branch.rb
+++ lib/option_branch.rb
@@ -7,9 +7,46 @@ module OptionBranch
 
   @argv = []
 
+  VALID_OPTIONS = %w(status merged).freeze
+
+  def usage
+    puts 'Usage: gith branch [option]'
+    puts "\n[options]\n\n"
+    puts option_status
+    puts option_merged
+    puts " By default, base_branch is 'master'"
+
+    abort
+  end
+
+  def option_status
+    %( status \t $ gith branch status
+  \t\t - Show status of all branchs
+
+  \t\t $ gith branch status [branch_name_1] ... [branch_name_n]
+  \t\t - Show status of specified branchs
+    )
+  end
+
+  def option_merged
+    %( merged \t $ gith merged
+  \t\t - Show all branchs that was merged on base_branch
+
+  \t\t - $ gith branch merged [base_branch]
+  \t\t - Show all branchs that was merged on specified base_branch
+    )
+  end
+
   def run(argv)
     @argv = argv
 
+    usage if @argv.size < 2 || !VALID_OPTIONS.include?(@argv[1])
+
+    option = @argv[1]
+    send(option)
+  end
+
+  def status
     branch_names, base_branch_name = branchs_names
 
     branch_names.each do |branch_name|
@@ -18,17 +55,18 @@ module OptionBranch
     end
   end
 
-  def usage
-    puts 'Usage: gith branch [branch_name] [base_branch_name]'
-    puts "\n by default, base_branch_name is 'master'"
+  def merged
+    base_branch_name = 'master' if ARGV.count < 3
+    base_branch_name ||= ARGV[2]
 
-    abort
+    merged_branchs = Githelper.all_merged(base_branch_name)
+    merged_branchs.each { |branch_name| puts branch_name }
   end
 
   def branchs_names
-    if @argv.count > 1
-      branch_names = [@argv[1]]
-      base_branch_name = @argv[2] || 'master'
+    if @argv.count > 2
+      branch_names = [@argv[2]]
+      base_branch_name = @argv[3] || 'master'
     else
       base_branch_name = 'master'
       branch_names = Githelper.get_all_branchs(base_branch_name)
@@ -57,13 +95,5 @@ module OptionBranch
       puts 'Status: OK'
     end
     puts "With: #{base_branch_name}"
-  end
-
-  def merged
-    base_branch_name = 'master' if @argv.count < 2
-    base_branch_name ||= @argv[1]
-
-    merged_branchs = Githelper.all_merged(base_branch_name)
-    merged_branchs.each { |branch_name| puts branch_name }
   end
 end
